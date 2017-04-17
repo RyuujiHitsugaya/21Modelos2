29a12b6696d246d6080fad5a1a8c01e44fc93c9d
Brallan Mendoza <keidragon14@gmail.com>
2017-04-17 17:49:22 -0500

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,24 +1,24 @@
 import random
 def mazo():
-    return [(numero,pinta) for numero in ['2','3','4','5','6','7','8','9','10','J','Q','K','A'] for pinta in ['♥','♦','♣','♥']]
+    return [(numero,pinta) for numero in [2,3,4,5,6,7,8,9,10,'J','Q','K','A'] for pinta in ['♥','♦','♣','♥']]
 def valorDe(carta,valor):
-    if (carta[0]=='2'):
+    if (carta[0]==2):
         return 2
-    if (carta[0]=='3'):
+    if (carta[0]==3):
         return 3
-    if (carta[0]=='4'):
+    if (carta[0]==4):
         return 4
-    if (carta[0]=='5'):
+    if (carta[0]==5):
         return 5
-    if (carta[0]=='6'):
+    if (carta[0]==6):
         return 6
-    if (carta[0]=='7'):
+    if (carta[0]==7):
         return 7
-    if (carta[0]=='8'):
+    if (carta[0]==8):
         return 8
-    if (carta[0]=='9'):
+    if (carta[0]==9):
         return 9
-    if (carta[0]=='10'):
+    if (carta[0]==10):
         return 10
     if (carta[0]=='J'):
         return 10
@@ -32,6 +32,8 @@ def valorDe(carta,valor):
         else:
             return 11
 def obtenerValor(mano):
+    return obtenerValorOrden(sorted(mano, key=str))
+def obtenerValorOrden(mano):
     if (mano==[]):
         return 0
     else:
@@ -44,8 +46,8 @@ def tomarCarta(mazo):
         return []
 def jugar(mazo,manoJugador,manoPc):
     print("Tienes las cartas: ",manoJugador)
-    print("La suma de tus cartas es: ",obtenerValor(manoJugador))    
     print("La banca tiene la carta: ",manoPc[0])
+    print("La suma de tus cartas es: ",obtenerValor(manoJugador))     
     if(obtenerValor(manoJugador)<21):
         if (input("¿Desea tomar otra carta?(s/n)")==('s' or 'S')):
             jugar(mazo,manoJugador+tomarCarta(mazo),manoPc)
@@ -59,25 +61,10 @@ def Juego(mazo,manoJugador,manoPc):
     if(obtenerValor(manoJugador)<=21):
         if(obtenerValor(manoPc)>21):       
             print("GANASTE con: ",manoJugador)
-        elif (obtenerValor(manoJugador)==21 and len(manoJugador)==2):
-            if(obtenerValor(manoPc)==21 and len(manoPc)==2):
-                print("EMPATE")
-            else:
-                print("GANASTE con: ",manoJugador)
-        elif(obtenerValor(manoJugador)>obtenerValor(manoPc)):
-            Juego(mazo,manoJugador,manoPc+tomarCarta(mazo)) 
-        elif(obtenerValor(manoPc)==obtenerValor(manoJugador)):
-            if(len(manoPc)==len(manoJugador)):
-                if(obtenerValor(manoJugador==21)):
-                    print("EMPATE")
-                else:
-                    Juego(mazo,manoJugador,manoPc+tomarCarta(mazo)) 
-            elif(len(manoPc)>len(manoJugador)):            
-                print("GANASTE con: ",manoJugador)
-            else:            
-                print("PERDISTE")
-        else:
+        elif(obtenerValor(manoPc)>=obtenerValor(manoJugador)):
             print("PERDISTE")
+        else:
+            Juego(mazo,manoJugador,manoPc+tomarCarta(mazo))
     else:
         print("PERDISTE")            
 def iniciar(mazo):
